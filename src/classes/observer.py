b8561d4ddbc9771c6677aa4a365e75cdf326ea23
Patrickwalsh33 <patrickwalsh33@outlook.com>
2025-02-18 11:17:37 +0000

    Refactor/comments

--- src/classes/observer.py
+++ src/classes/observer.py
@@ -1,28 +1,22 @@
 from abc import ABC, abstractmethod
 
 class Observer(ABC):
-    """Abstract base class for observers"""
     @abstractmethod
     def update(self, subject, post=None):
-        """Method that observers must implement to receive updates"""
         pass
 
 class Subject(ABC):
-    """Abstract base class for subjects"""
     def __init__(self):
         self._observers = []
 
     @abstractmethod
     def attach(self, observer: Observer):
-        """Attach an observer to the subject"""
         pass
 
     @abstractmethod
     def detach(self, observer: Observer):
-        """Detach an observer from the subject"""
         pass
 
     @abstractmethod
     def notify(self, post=None):
-        """Notify all observers about an event"""
         pass
