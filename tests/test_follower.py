7f65a00114c3c7a32d8e90e4d81ba3620d19b340
dylan-teehan-skc <dylan.teehan@gmail.com>
2025-02-18 11:20:48 +0000

    Feat/implement testing

---
+++ tests/test_follower.py
@@ -0,0 +1,20 @@
+import unittest
+from src.classes.follower import Follower
+from src.classes.post import Sentiment
+
+class TestFollower(unittest.TestCase):
+
+    def test_initial_sentiment_adjustment_left(self):
+        follower = Follower(Sentiment.LEFT)
+        self.assertLessEqual(follower.political_lean, 50, "Political lean should be adjusted towards left")
+
+    def test_initial_sentiment_adjustment_right(self):
+        follower = Follower(Sentiment.RIGHT)
+        self.assertGreaterEqual(follower.political_lean, 50, "Political lean should be adjusted towards right")
+
+    def test_initial_sentiment_adjustment_neutral(self):
+        follower = Follower(Sentiment.NEUTRAL)
+        self.assertEqual(follower.political_lean, 50, "Political lean should remain neutral")
+
+if __name__ == '__main__':
+    unittest.main() 
