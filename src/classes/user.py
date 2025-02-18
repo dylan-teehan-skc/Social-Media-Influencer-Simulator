0a0846b2e677826ecf97ff84052adc506db3b5f3
Patrickwalsh33 <patrickwalsh33@outlook.com>
2025-02-18 10:53:45 +0000

    comments

--- src/classes/user.py
+++ src/classes/user.py
@@ -1,9 +1,8 @@
-from observer import Observer, Subject  # Importing Observer and Subject
+from observer import Subject 
 
 class User(Subject):
     def __init__(self, handle, bio):
-        super().__init__()  # Initialize the Subject class
-        # Attributes
+        super().__init__()  
         self.handle = handle
         self.bio = bio
         self.followers = 0
@@ -11,25 +10,19 @@ class User(Subject):
         self.posts = []
 
     def create_post(self, content):
-        # Implementation for creating a post
         pass
 
     def edit_post(self, post):
-        # Implementation for editing a post
         pass
 
     def delete_post(self, post):
-        # Implementation for deleting a post
         pass
 
     def follow(self, user):
-        # Implementation for following a user
         pass
 
     def unfollow(self, user):
-        # Implementation for unfollowing a user
         pass
 
     def update_credibility(self):
-        # Implementation for updating credibility score
         pass
