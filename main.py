e8b7ae032a07d90f93a25f227ca537d60929752e
Patrickwalsh33 <147488089+Patrickwalsh33@users.noreply.github.com>
2025-03-03 10:22:52 +0000

    Refactor/moving UI logic (#20)
    * sloggo farted
    
    * mediator first draft

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from lib_config.config_loader import ConfigLoader
 from lib_utils.logger import setup_logger
 from src.services.logger_service import LoggerService
-from src.game.game_manager import GameManager
 import logging
+from src.mediator.mediator import Mediator
+from frontend.ui_logic import UILogic
 
 def main():
     # Load configuration
@@ -17,10 +18,13 @@ def main():
     # Set the logger in the service and configure debug level
     LoggerService.set_logger(logger)
     logger.setLevel(logging.DEBUG)
+
+    mediator = Mediator()
+
+    ui_logic = UILogic(mediator)
     
     # Create and run game
-    game = GameManager()
-    game.run()
+    ui_logic.run()
 
 if __name__ == "__main__":
     main() 
