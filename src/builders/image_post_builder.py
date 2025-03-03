be0065281a1ac7269dc8c90fe464b24d22d80b07
Dylan Teehan <96421021+dylan-teehan-skc@users.noreply.github.com>
2025-03-03 11:34:19 +0000

    Feat/adding_logging (#21)
    * Feat/adding_logging
    
    * Refactor/remove commands
    
    * Refactor/rename variables

--- src/builders/image_post_builder.py
+++ src/builders/image_post_builder.py
@@ -1,18 +1,24 @@
 from src.builders.post_builder import PostBuilder
 from src.models.post import Post
 from src.models.user import User
+from src.services.logger_service import LoggerService
 
 class ImagePostBuilder(PostBuilder):
     def __init__(self):
         self.post = Post("")
+        self.logger = LoggerService.get_logger()
+        self.logger.debug("Initialized ImagePostBuilder")
 
     def set_content(self, content: str) -> 'ImagePostBuilder':
         self.post.content = f"Image: {content}"
+        self.logger.debug("Set image post content with path: %s", content)
         return self
 
     def set_author(self, author: User) -> 'ImagePostBuilder':
         self.post.author = author
+        self.logger.debug("Set image post author: %s", author.handle)
         return self
 
     def build(self) -> Post:
+        self.logger.info("Built image post for author %s", self.post.author.handle if self.post.author else "Unknown")
         return self.post 
--- src/factory/post_builder_factory.py
+++ src/factory/post_builder_factory.py
@@ -1,13 +1,21 @@
 from src.builders.text_post_builder import TextPostBuilder
 from src.builders.image_post_builder import ImagePostBuilder
 from src.builders.post_builder import PostBuilder
+from src.services.logger_service import LoggerService
 
 class PostBuilderFactory:
+    logger = LoggerService.get_logger()
+
     @staticmethod
     def get_builder(post_type: str) -> PostBuilder:
+        PostBuilderFactory.logger.debug("Requesting builder for post type: %s", post_type)
+        
         if post_type == "text":
+            PostBuilderFactory.logger.info("Creating TextPostBuilder")
             return TextPostBuilder()
         elif post_type == "image":
+            PostBuilderFactory.logger.info("Creating ImagePostBuilder")
             return ImagePostBuilder()
         else:
+            PostBuilderFactory.logger.error("Unknown post type requested: %s", post_type)
             raise ValueError("Unknown post type") 
